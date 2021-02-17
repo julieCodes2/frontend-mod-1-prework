3f38b1fdc7311696a80f1a8b0cde3aca508d9347
Julie Zeiger <72552438+julieCodes2@users.noreply.github.com>
2021-02-17 16:58:35 +0100

    Update modZeroHero.js
    Julie's progress so far...I will continue to update this though.

--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,45 +1,83 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+// Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
+// Declare two variables - heroName AND specialAbility - set to strings
+var heroName = " ";
+var specialAbility = " ";
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
-
+var greeting = " " + heroName;
+var catchphrase = "We have ${specialAbility}";
 // Declare two variables - power AND energy - set to integers
-
+var power = 1;
+var energy = 2;
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
-
+var fullPower = 1 * 500;
+var fullEnergy = 2 + 150;
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
+var isHuman = true;
+var identityConcealed = false;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
-
+var archEnemies = ["Enemy1", "Enemy2", "Enemy3"];
+var sidekicks = ["Sidekick1", "Sidekick2", "Sidekick3"];
 // Print the first sidekick to your console
-
+console.log(sidekicks[0]);
 // Print the last archEnemy to the console
-
+console.log(archEnemies[2]);
 // Write some code to add a new archEnemy to the archEnemies array
-
+var moreArchEnemies = archEnemies.push("Enemy4");
 // Print the archEnemies array to console to ensure you added a new archEnemy
-
+console.log(archEnemies);
 // Remove the first sidekick from the sidekicks array
+varRemoveSidekick = sidekicks.shift()
 
 // Print the sidekicks array to console to ensure you added a new sidekick
+var addSidekick = sidekicks.push("Sidekick4");
+console.log(sidekicks);
 
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
 //   - saveTheDay should be a string a hero would say once they save the day 
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
-
+var assessSituation = function() {
+dangerLevel = 1;
+saveTheDay = "Mission Accomplished";
+badExcuse = "This is dangerous";
+}
+if (dangerLevel > 50) {
+  console.log(+ badExcuse);
+} else if (dangerLevel === 10 || dangerLevel <= 50){
+  console.log(+ badExcuse);
+}else if (dangerLevel < 10) {
+  console.log("Meh. Hard pass.");
+}
+console.log(assessSituation);
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
+/*to be edited Draft Draft
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+
+if (dangerLevel > 50) {
+  console.log("This is dangerous");
+} else if (dangerLevel === 10 || dangerLevel <= 50){
+  console.log("Mission Accomplished");
+}else if (dangerLevel < 10) {
+  console.log("Meh. Hard pass");
+}
+}
+assessSituation(9);
+//to be edited. Draft Draft
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!'
